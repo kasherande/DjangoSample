3c8e661d3a249677f65fab2591dff6a263d0bbc4
kasherande <kasherande@gmail.com>
2023-01-17 09:47:56 +0530

    Added the address and author table

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -1,13 +1,19 @@
 from django.contrib import admin
-from .models import Book
+from .models import Address, Book, Author
 # Register your models here.
 
 
 class BookAdmin(admin.ModelAdmin):
     # readonly_fields = ("slug",)
     prepopulated_fields = {"slug": ("title", )}
-    list_filter = ("author", "rating",)
+    list_filter = ("author", "rating")
     list_display = ("title", "author")
 
+# class AuthorAdmin(admin.ModelAdmin):
+    # list_filter = ("author","address")
+    # list_display = ("author","address")
 
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
+# admin.site.register(Author, AuthorAdmin)
+admin.site.register(Address)
