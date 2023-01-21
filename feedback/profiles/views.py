9047a21a07358b565837f6a5b8dc4a9cac19bc13
kasherande <kasherande@gmail.com>
2023-01-21 18:32:57 +0530

    Added the feedback user profile

--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -1,12 +1,42 @@
 from django.shortcuts import render
 from django.views import View
-
+from django.http import HttpResponseRedirect
+from django.views.generic.edit import CreateView
+from .forms import ProfileForm
+from .models import UserProfile
+from django.views.generic import ListView
 # Create your views here.
 
 
-class CreateProfileView(View):
-    def get(self, request):
-        return render(request, "profiles/create_profile.html")
+def store_file(file):
+    with open("temp/image.jpg", "wb+") as dest:
+        for chunk in file.chunks():
+            dest.write(chunk)
 
-    def post(self, request):
-        pass
+class CreateProfileView(CreateView):
+    template_name = "profiles/create_profile.html"
+    model = UserProfile
+    fields = "__all__"
+    success_url = "/profiles"
+
+class ProfilesView(ListView):
+    template_name = "profiles/user_profiles.html"
+    model = UserProfile
+    context_object_name="profiles"
+
+# class CreateProfileView(View):
+    # def get(self, request):
+    #     form = ProfileForm()
+    #     return render(request, "profiles/create_profile.html",{
+    #         "form": form
+    #     })
+
+    # def post(self, request):
+    #     submitted_form = ProfileForm(request.POST, request.FILES)
+    #     if submitted_form.is_valid():
+    #         profile =  UserProfile(image = request.FILES["user_image"])
+    #         profile.save()
+    #         return HttpResponseRedirect("/profiles")
+    #     return render(request, "profiles/create_profile.html",{
+    #         "form": submitted_form
+    #     })
