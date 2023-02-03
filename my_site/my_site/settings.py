b616d8069aea0d3f4db2ed40954e4c789d63120a
kasherande <kasherande@gmail.com>
2023-02-03 17:01:25 +0530

    Install Mysql client

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -79,9 +79,20 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my_site',  
+        'USER': 'admin',  
+        'PASSWORD': 'Admin123',
+        'HOST': 'my-site.cwg4sd4qknft.ap-south-1.rds.amazonaws.com',  
+        'PORT': '3306',
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        }  
     }
 }
 
